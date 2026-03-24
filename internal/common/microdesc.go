f0b1cee0e2464bb1290ca731f989a00c2580fcd6
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-03-24 10:21:27 -0300

    feat: parse family-ids line

--- internal/common/microdesc.go
+++ internal/common/microdesc.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"encoding/base64"
 	"encoding/hex"
 	"strings"
 )
@@ -10,18 +11,46 @@ type Microdesc struct {
 	NTorOnionKey []byte
 	IdEd25519    []byte
 	Family       [][]byte
+	Familys      []*FamilyIDs
+}
+
+type FamilyIDs struct {
+	Kind  string
+	Value []byte
 }
 
 func parseMicrodescBlock() {
 
 }
 
+func parseFamilys(s string) (ids []*FamilyIDs, err error) {
+	split := strings.SplitSeq(s, " ")
+
+	for str := range split {
+
+		id := strings.SplitN(str, ":", 2)
+
+		a := &FamilyIDs{
+			Kind: id[0],
+		}
+		var err error
+		a.Value, err = base64.RawStdEncoding.DecodeString(id[1])
+		if err != nil {
+			return nil, err
+		}
+
+		ids = append(ids, a)
+	}
+
+	return nil, nil
+}
+
 func parseFamily(s string) (family [][]byte, err error) {
 	split := strings.SplitSeq(s, " ")
 
-	for v := range split {
-		v = strings.TrimPrefix(v, "$")
-		b, err := hex.DecodeString(v)
+	for str := range split {
+		str = strings.TrimPrefix(str, "$")
+		b, err := hex.DecodeString(str)
 		if err != nil {
 			return nil, err
 		}
