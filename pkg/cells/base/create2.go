2098ca9133a7d20151418712bdf8ca5c28cb1662
robogg133 <luizfernandojoaquimdasilva@proton.me>
2026-05-31 02:38:54 -0300

    feat: CREATE2

---
+++ pkg/cells/base/create2.go
@@ -0,0 +1,37 @@
+package cells
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+
+	"github.com/robogg133/gonion/pkg/handshakes"
+)
+
+const COMMAND_CREATE2 uint8 = 10
+
+type Create2Cell struct {
+	CircuitID uint32
+
+	HandshakeType uint16
+	Handshake     handshakes.Handshake
+}
+
+func (*Create2Cell) ID() uint8               { return COMMAND_CREATE2 }
+func (c *Create2Cell) GetCircuitID() uint32  { return c.CircuitID }
+func (c *Create2Cell) SetCircuitID(n uint32) { c.CircuitID = n }
+
+func (c *Create2Cell) Encode(w io.Writer) error {
+	var buffer bytes.Buffer
+	if err := c.Handshake.Encode(&buffer); err != nil {
+		return err
+	}
+	hs := buffer.Bytes()
+
+	binary.Write(w, binary.BigEndian, c.HandshakeType)
+	binary.Write(w, binary.BigEndian, uint16(len(hs)))
+	_, err := w.Write(hs)
+	return err
+}
+
+func (c *Create2Cell) Decode(io.Reader) error { return nil }
