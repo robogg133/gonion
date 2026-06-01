6d5fec19b726a1029dfdfbeb2c011a608f5745f8
robogg133 <luizfernandojoaquimdasilva@proton.me>
2026-06-01 19:37:48 -0300

    feat: added selection with weigth but not restrictions

--- pkg/path/guard.go
+++ pkg/path/guard.go
@@ -3,9 +3,17 @@ package path
 import "github.com/robogg133/gonion/pkg/common"
 
 func guardWeightFunc(flags [15]bool, weights common.BandWidthWeight) int64 {
-	var weightToUse int64 = int64(weights.Wee)
-	if flags[common.FLAG_GUARD] {
-		weightToUse = int64(weights.Wed)
+	var weightToUse int64 = int64(weights.Wgg)
+	if flags[common.FLAG_EXIT] {
+		weightToUse = int64(weights.Wgd)
 	}
 	return weightToUse
+}
+
+func guardValideFunc(r common.RouterStatus) bool {
+	if r.NTorOnionKey == nil {
+		return false
+	}
+
+	return r.StatusFlags[common.FLAG_GUARD] && r.StatusFlags[common.FLAG_FAST] && r.StatusFlags[common.FLAG_STABLE] && r.StatusFlags[common.FLAG_V2DIR]
 }
--- pkg/path/path.go
+++ pkg/path/path.go
@@ -3,20 +3,21 @@ package path
 
 import (
 	"errors"
+	"fmt"
 	"math/rand/v2"
 
 	"github.com/robogg133/gonion/pkg/common"
 )
 
 type Selector struct {
-	list       []common.RouterStatus
-	weight     *common.BandWidthWeight
-	longLive   bool
-	targetExit uint16
+	list     []common.RouterStatus
+	weight   common.BandWidthWeight
+	longLive bool
 
-	guard   *common.RouterStatus
-	middles []*common.RouterStatus
-	exit    *common.RouterStatus
+	guard    *common.RouterStatus
+	middles  []*common.RouterStatus
+	exit     *common.RouterStatus
+	fullPath []*common.RouterStatus
 }
 
 type value struct {
@@ -27,19 +28,81 @@ type value struct {
 type validateFunc func(r common.RouterStatus) bool
 type weightFunc func(flags [15]bool, weights common.BandWidthWeight) int64
 
-func (sl *Selector) selectRelay(fn validateFunc, wfn weightFunc) (*common.RouterStatus, error) {
+func New(cns *common.Consensus, longlive bool) *Selector {
+	return &Selector{
+		list:     cns.RelayInformation,
+		weight:   cns.BandWidthWeight,
+		longLive: longlive,
+	}
+}
+
+func (sl *Selector) SelectRandomCircuit(hops uint, port uint16) error {
+	if hops <= 0 {
+		return fmt.Errorf("invalid number of hops: %d need to be greater than 0", hops)
+	}
+
+	exitInfo, err := sl.selectRelay(exitValidateFunc, exitWeightFunc, port)
+	if err != nil {
+		return err
+	}
+	sl.exit = exitInfo
+	hops--
+
+	if hops <= 0 {
+		sl.fullPath = append(sl.fullPath, exitInfo)
+		return nil
+	}
+
+	guardInfo, err := sl.selectRelay(guardValideFunc, guardWeightFunc, 0)
+	if err != nil {
+		return err
+	}
+	sl.guard = guardInfo
+	sl.fullPath = append(sl.fullPath, guardInfo)
+	hops--
+
+	for range hops {
+		middleInfo, err := sl.selectRelay(middleValideFunc, middleWeightFunc, 0)
+		if err != nil {
+			return err
+		}
+		sl.middles = append(sl.middles, middleInfo)
+		sl.fullPath = append(sl.fullPath, middleInfo)
+	}
+	sl.fullPath = append(sl.fullPath, exitInfo)
+	return nil
+}
+
+func (sl *Selector) Guard() *common.RouterStatus    { return sl.guard }
+func (sl *Selector) Exit() *common.RouterStatus     { return sl.exit }
+func (sl *Selector) Middle() []*common.RouterStatus { return sl.middles }
+
+func (sl *Selector) Circuit() []*common.RouterStatus { return sl.fullPath }
+
+func (sl *Selector) selectRelay(fn validateFunc, wfn weightFunc, desiredPort uint16) (*common.RouterStatus, error) {
 	var totalBw int64
 	var values []value
 
 	for _, v := range sl.list {
+
+		if desiredPort != 0 {
+			if !v.Ports.IsAllowed(desiredPort) {
+				continue
+			}
+		}
+
 		if sl.longLive && !v.StatusFlags[common.FLAG_STABLE] {
 			continue
 		}
+		if !v.StatusFlags[common.FLAG_RUNNING] && !v.StatusFlags[common.FLAG_VALID] {
+			continue
+		}
+
 		if !fn(v) {
 			continue
 		}
 
-		w := int64(v.BandWidth) * wfn(v.StatusFlags, *sl.weight) / 10000
+		w := int64(v.BandWidth) * wfn(v.StatusFlags, sl.weight) / 10000
 		totalBw += w
 		values = append(values, value{wb: w, ptr: &v})
 	}
