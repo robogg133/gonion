7b1170df72d1438ef77ea9203692cf65b4d0ef79
robogg133 <luizfernandojoaquimdasilva@proton.me>
2026-06-05 01:55:17 -0300

    ref: embeding relaycell in RelayEarlyCell

--- pkg/cells/base/relay_early.go
+++ pkg/cells/base/relay_early.go
@@ -1,5 +1,16 @@
 package cells
 
+import "io"
+
 const COMMAND_RELAY_EARLY uint8 = 9
 
-type RelayEarlyCell RelayCell
+type RelayEarlyCell struct {
+	c *RelayCell
+}
+
+func (*RelayEarlyCell) ID() uint8               { return COMMAND_RELAY_EARLY }
+func (c *RelayEarlyCell) GetCircuitID() uint32  { return c.c.CircuitID }
+func (c *RelayEarlyCell) SetCircuitID(n uint32) { c.c.CircuitID = n }
+
+func (c *RelayEarlyCell) Encode(w io.Writer) error { return c.c.Encode(w) }
+func (c *RelayEarlyCell) Decode(r io.Reader) error { return c.c.Decode(r) }
