9b9cc97104d8922919ade93ca44ff7341a7e67fd
robogg133 <luizfernandojoaquimdasilva@proton.me>
2026-06-05 01:34:07 -0300

    feat: extend2 cell

---
+++ pkg/cells/relay/extend2.go
@@ -0,0 +1,54 @@
+package relay
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+
+	"github.com/robogg133/gonion/pkg/handshakes"
+	"github.com/robogg133/gonion/pkg/lspec"
+)
+
+const COMMAND_EXTEND2 uint8 = 14
+
+type Extend2Cell struct {
+	CircuitID uint32
+	Lspecs    []lspec.Lspec
+
+	HType     uint8
+	Handshake handshakes.Handshake
+}
+
+func (*Extend2Cell) ID() uint8           { return COMMAND_EXTEND2 }
+func (*Extend2Cell) GetStreamID() uint16 { return 0 }
+func (*Extend2Cell) SetStreamID(uint16)  {}
+
+func (c *Extend2Cell) Encode(w io.Writer) error {
+	if err := binary.Write(w, binary.BigEndian, c.CircuitID); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.BigEndian, uint8(len(c.Lspecs))); err != nil {
+		return err
+	}
+
+	for _, v := range c.Lspecs {
+		if err := v.Write(w); err != nil {
+			return err
+		}
+	}
+
+	var buffer bytes.Buffer
+	c.Handshake.Encode(&buffer)
+
+	w.Write([]byte{c.HType})
+
+	binary.Write(w, binary.BigEndian, uint16(buffer.Len()))
+
+	_, err := w.Write(buffer.Bytes())
+	return err
+}
+
+func (*Extend2Cell) Decode(r io.Reader) error {
+	// TODO
+	return nil
+}
