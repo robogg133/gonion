87c5453b464b224dabdca7badb245cab30065ce0
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-07-05 21:25:09 -0300

    feat: added new helpers for sendme

--- helpers.go
+++ helpers.go
@@ -1,6 +1,7 @@
 package gonion
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/robogg133/gonion/internal/window"
@@ -24,4 +25,41 @@ func verifySendMe(sendme *relay.SendMeCell, sendmeVersion uint8, window *window.
 		return fmt.Errorf("protocol violation: unexpected SEND_ME")
 	}
 	return nil
+}
+
+func (c *Circuit) sendmeManage(i int, window *window.Window, ctx context.Context) {
+	for {
+		select {
+		case digest := <-window.Get():
+			sendMeCell := &relay.SendMeCell{
+				StreamID:        0,
+				Version:         c.SendMeVersion,
+				Sha1ForLastCell: digest,
+			}
+			select {
+			case c.WriteRelayCell <- struct {
+				relay.Cell
+				uint8
+			}{Cell: sendMeCell, uint8: uint8(i)}:
+				window.Increase()
+			case <-ctx.Done():
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (c *Circuit) deleteHop(i int) {
+	c.hops = remove(c.hops, i)
+	c.hopsWindows = remove(c.hopsWindows, i)
+	c.hopsCtx = remove(c.hopsCtx, i)
+}
+
+func remove[T any](s []T, i int) []T {
+	if i < 0 || i >= len(s) {
+		return s
+	}
+	return append(s[:i], s[i+1:]...)
 }
